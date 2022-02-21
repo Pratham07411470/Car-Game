c2d0e21b9a36633e33b64545fdb2cded4f76355b
Pratham07411470 <100145710+Pratham07411470@users.noreply.github.com>
2022-02-21 20:59:28 +0530

    Add files via upload

---
+++ scripts/Photon/TestConnect.cs
@@ -0,0 +1,29 @@
+﻿using Photon.Pun;
+using Photon.Realtime;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestConnect : MonoBehaviourPunCallbacks
+{
+   private void Start()
+   {
+        print("connecting to server.");
+        PhotonNetwork.NickName = MasterManager.GameSettings.NickName;
+        PhotonNetwork.GameVersion = MasterManager.GameSettings.GameVersion;
+        PhotonNetwork.ConnectUsingSettings();
+
+   }
+
+    public override void OnConnectedToMaster()
+    {
+        print("connected to server.");
+        print(PhotonNetwork.LocalPlayer.NickName);
+        PhotonNetwork.JoinLobby();
+    }
+
+    public override void OnDisconnected(DisconnectCause cause)
+    {
+        print("DIsconnected from server for some reason" + cause.ToString());
+    }
+}
